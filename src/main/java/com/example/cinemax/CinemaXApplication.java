99b9d07c42f4e962153fae43b3455ac58446c2b3
Dzhos Oleksii <dzhos.oleksii@gmail.com>
2022-09-28 18:11:24 +0300

    init project

---
+++ src/main/java/com/example/cinemax/CinemaXApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinemax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaXApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cinemax/CinemaXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinemax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
