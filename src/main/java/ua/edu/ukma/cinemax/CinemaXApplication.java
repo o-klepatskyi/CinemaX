ecc4f0ce6540fc4aa9bbb14e02a58b48a99f8c34
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2022-10-25 17:41:58 +0300

    Profile dev on test method

--- src/main/java/ua/edu/ukma/cinemax/CinemaXApplication.java
+++ src/main/java/ua/edu/ukma/cinemax/CinemaXApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import ua.edu.ukma.cinemax.model.Film;
 import ua.edu.ukma.cinemax.service.FilmService;
 
@@ -13,15 +14,20 @@ public class CinemaXApplication {
     public static void main(String[] args) {
         SpringApplication.run(CinemaXApplication.class, args);
     }
-
-//    @Bean
-////    public CommandLineRunner run(FilmService filmService) {
-////        return (String[] args) -> {
-////            Film shrek = new Film();
-////            shrek.setTitle("Shrek");
-////            shrek.setReleaseYear(2001);
-////            shrek.setDescription("Amazing start of the 21st century!");
-////            filmService.add(shrek);
-////        };
-////    }
+    @Bean
+    @Profile("dev")
+    public CommandLineRunner run(FilmService filmService) {
+        return (String[] args) -> {
+            Film shrek = new Film();
+            shrek.setTitle("Shrek");
+            shrek.setReleaseYear(2001);
+            shrek.setDescription("Amazing start of the 21st century!");
+            filmService.add(shrek);
+            Film test = new Film();
+            test.setTitle("Test film");
+            test.setReleaseYear(9999);
+            test.setDescription("Testing description...");
+            filmService.add(test);
+       };
+   }
 }
