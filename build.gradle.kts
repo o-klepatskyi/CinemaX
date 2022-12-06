059e00361bc0192e1fe0f0c3723f140d60575327
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2022-12-06 21:09:07 +0200

    Spring security test config

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,14 +38,15 @@ dependencies {
     implementation("org.apache.commons:commons-dbcp2:2.9.0")
     implementation("com.h2database:h2:1.4.200")
     implementation ("org.springframework.boot:spring-boot-starter-validation:2.4.0")
+
     compileOnly("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:4.9.0")
-
+    testImplementation("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.mockito:mockito-junit-jupiter")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
