584ba29bac1efcee90cd951ef4a6222de3c752f5
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2022-12-06 20:18:33 +0200

    Mockito works

--- src/test/java/ua/edu/ukma/cinemax/FilmControllerTest.java
+++ src/test/java/ua/edu/ukma/cinemax/FilmControllerTest.java
@@ -1,33 +1,29 @@
 package ua.edu.ukma.cinemax;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hamcrest.core.StringContains;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import ua.edu.ukma.cinemax.api.controller.FilmController;
+import ua.edu.ukma.cinemax.service.FilmService;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = FilmController.class)
-//@SpringBootTest
 public class FilmControllerTest {
-//    @Autowired
-//    private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-//    @Autowired
-//    private ObjectMapper objectMapper;
+    @MockBean
+    private FilmService filmService;
 
-//    @Autowired
-//    private FilmController filmController;
-
+    @Test
+    public void anonymousAccessToAllFilms() throws Exception {
+        mockMvc.perform(get("/api/film/all")).andDo(print()).andExpect(status().isOk());
+    }
 
 }
+
