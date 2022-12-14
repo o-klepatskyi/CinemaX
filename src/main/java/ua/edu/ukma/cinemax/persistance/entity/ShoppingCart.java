a88ab4dd08ab377635302be0427feb838259c791
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2022-12-14 20:00:02 +0200

    Update ShoppingCart.java

--- src/main/java/ua/edu/ukma/cinemax/persistance/entity/ShoppingCart.java
+++ src/main/java/ua/edu/ukma/cinemax/persistance/entity/ShoppingCart.java
@@ -1,28 +1,40 @@
 package ua.edu.ukma.cinemax.persistance.entity;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
 import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MapsId;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Objects;
 
 @Entity
 @Table(name = "shopping_carts")
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class ShoppingCart extends AbstractEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany
+    @ToString.Exclude
     private List<Ticket> tickets;
     @OneToOne
     @MapsId
     private User user;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        ShoppingCart that = (ShoppingCart) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
