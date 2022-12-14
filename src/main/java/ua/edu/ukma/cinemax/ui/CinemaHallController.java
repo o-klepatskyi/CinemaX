bb96de0e5481d7ddd762892486835b86295771e7
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2022-12-14 10:57:58 +0200

    Cinema Hall + Session controllers and pages

---
+++ src/main/java/ua/edu/ukma/cinemax/ui/CinemaHallController.java
@@ -0,0 +1,80 @@
+package ua.edu.ukma.cinemax.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.ModelAndView;
+import ua.edu.ukma.cinemax.api.model.ApiCinemaHall;
+import ua.edu.ukma.cinemax.dto.FilmDto;
+import ua.edu.ukma.cinemax.persistance.entity.CinemaHall;
+import ua.edu.ukma.cinemax.persistance.entity.Film;
+import ua.edu.ukma.cinemax.service.CinemaHallService;
+
+@Controller
+@RequiredArgsConstructor
+public class CinemaHallController {
+    private final CinemaHallService cinemaHallService;
+
+    @GetMapping("/cinema-hall/add")
+    public String add(Model model) {
+        ApiCinemaHall apiCinemaHall = new ApiCinemaHall();
+        model.addAttribute("cinemaHall", apiCinemaHall);
+        return "cinema-hall/add";
+    }
+
+    @PostMapping("/cinema-hall/add")
+    public String submitNewCinemaHall(@Valid @RequestBody @ModelAttribute("cinema-hall") ApiCinemaHall cinemaHall,
+                                   BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("errorMessage", "Invalid cinema hall data");
+            return "cinema-hall/add";
+        }
+        cinemaHallService.add(cinemaHall.toModel());
+        return "redirect:/cinema-hall/add?success";
+    }
+
+    @GetMapping("/cinema-hall/all")
+    public String selectAll(Model model) {
+        List<CinemaHall> cinemaHalls = cinemaHallService.getAll();
+        List<ApiCinemaHall> apiCinemaHalls = new ArrayList<>(cinemaHalls.size());
+        cinemaHalls.forEach((ch) -> apiCinemaHalls.add(new ApiCinemaHall(ch)));
+        model.addAttribute("cinemaHalls", apiCinemaHalls);
+        return "cinema-hall/all";
+    }
+
+    @GetMapping(path = "/cinema-hall/edit/{id}")
+    public String getEditPage(@PathVariable Long id, Model model) {
+        CinemaHall cinemaHall = cinemaHallService.get(id);
+        model.addAttribute("cinemaHall", cinemaHall);
+        return "cinema-hall/edit";
+    }
+
+    @PostMapping(path = "/cinema-hall/edit/{id}")
+    public String edit(@Valid @ModelAttribute("cinemaHall") ApiCinemaHall cinemaHall,
+                       BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "cinema-hall/edit";
+        }
+//        model.addAttribute("cinemaHall", cinemaHall);
+        cinemaHallService.update(cinemaHall.toModel());
+        return String.format("redirect:/cinema-hall/edit/%s?success", cinemaHall.getId());
+    }
+    @DeleteMapping(path = "cinema-hall/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        cinemaHallService.delete(id);
+        return "cinema-hall/all";
+    }
+}
+
--- src/main/java/ua/edu/ukma/cinemax/ui/SessionController.java
+++ src/main/java/ua/edu/ukma/cinemax/ui/SessionController.java
@@ -3,69 +3,94 @@ package ua.edu.ukma.cinemax.ui;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
-import org.springframework.http.MediaType;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import ua.edu.ukma.cinemax.api.model.ApiSession;
+import ua.edu.ukma.cinemax.dto.FilmDto;
+import ua.edu.ukma.cinemax.persistance.entity.Film;
 import ua.edu.ukma.cinemax.persistance.entity.Session;
+import ua.edu.ukma.cinemax.service.CinemaHallService;
+import ua.edu.ukma.cinemax.service.FilmService;
 import ua.edu.ukma.cinemax.service.SessionService;
 
-@RestController
-@AllArgsConstructor
-@RequestMapping("/session")
+@Controller
+@RequiredArgsConstructor
 public class SessionController {
-    private static int requestId = 0;
-    final static Logger logger = LoggerFactory.getLogger(SessionController.class);
-
     private final SessionService sessionService;
+    private final FilmService filmService;
+    private final CinemaHallService cinemaHallService;
 
-    @PostMapping("/add")
-    public void add(@RequestBody ApiSession session) {
-        MDC.put("request_id", "session/add/:request_id: " + requestId++);
-        sessionService.add(session.toModel());
-        MDC.clear();
+    @GetMapping("/session/add")
+    public String add(Model model) {
+        ApiSession apiSession = new ApiSession();
+        model.addAttribute("session", apiSession)
+                .addAttribute("films", filmService.getAll())
+                .addAttribute("cinemaHalls", cinemaHallService.getAll());
+        return "session/add";
     }
 
-    @GetMapping("/all")
-    public List<ApiSession> all(Model model) {
+    @PostMapping("/session/add")
+    public String submitNewSession(@Valid @RequestBody @ModelAttribute("session") ApiSession session,
+                                BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("errorMessage", "Invalid session data");
+            return "session/add";
+        }
+        sessionService.add(session.toModel());
+        return "redirect:/session/add?success";
+    }
+
+    @GetMapping("/session/all")
+    public String selectAll(Model model) {
         List<Session> sessions = sessionService.get();
         List<ApiSession> apiSessions = new ArrayList<>(sessions.size());
-        sessions.forEach(s -> apiSessions.add(new ApiSession(s)));
+        sessions.forEach((s) -> apiSessions.add(new ApiSession(s)));
         model.addAttribute("sessions", apiSessions);
-        return apiSessions;
+        return "session/all";
     }
 
-    @GetMapping(
-            path = "/available/{id}/{date}",
-            produces = {MediaType.APPLICATION_JSON_VALUE})
-    public List<ApiSession> selectAllAvailable(@PathVariable Long id,
+    @GetMapping("/session/all/{id}/{date}")
+    public String selectAllAvailable(@PathVariable Long id,
                                                @PathVariable LocalDate date) {
+        ModelAndView mav = new ModelAndView("list-sessions");
         List<Session> sessions = sessionService.getAvailableSessions(id, date);
         List<ApiSession> apiSessions = new ArrayList<>(sessions.size());
-        sessions.forEach(s -> apiSessions.add(new ApiSession(s)));
-        return apiSessions;
+        sessions.forEach((s) -> apiSessions.add(new ApiSession(s)));
+        mav.addObject("sessions", apiSessions);
+        return "session/all";
     }
 
-    @PutMapping(path = "edit/{id}")
-    public void edit(@PathVariable Long id,
-                     @RequestBody ApiSession session) {
-        session.setId(id);
+    @GetMapping(path = "/session/edit/{id}")
+    public String getEditPage(@PathVariable Long id, Model model) {
+        Session session = sessionService.get(id);
+        model.addAttribute("session", session);
+        return "session/edit";
+    }
+
+    @PostMapping(path = "/session/edit/{id}")
+    public String edit(@Valid @ModelAttribute("session") ApiSession session,
+                       BindingResult result) {
+        if (result.hasErrors()) {
+            return "session/edit";
+        }
         sessionService.update(session.toModel());
+        return String.format("redirect:/session/edit/%s?success", session.getId());
     }
 
-    @DeleteMapping(path = "delete/{id}")
-    public void delete(@PathVariable Long id) {
+    @DeleteMapping(path = "/session/delete/{id}")
+    public String delete(@PathVariable Long id) {
         sessionService.delete(id);
+        return "session/all";
     }
 }
