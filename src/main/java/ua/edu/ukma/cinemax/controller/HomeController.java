da6b0a42a96e1273ca6bb2f8a1e2fdacb3e0d28b
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2022-12-15 09:16:39 +0200

    default home page - film/all

--- src/main/java/ua/edu/ukma/cinemax/controller/HomeController.java
+++ src/main/java/ua/edu/ukma/cinemax/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping({"/", "/index"})
     public String home() {
-        return "/index";
+        return "redirect:/film/all";
     }
 }
