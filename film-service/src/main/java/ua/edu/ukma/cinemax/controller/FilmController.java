085f95ead75358923ef166c4a48b46aff2d3d9fc
Oleg Zagryvyi <olegzagraf@gmail.com>
2023-10-06 12:37:10 +0300

    add json/html mime

--- film-service/src/main/java/ua/edu/ukma/cinemax/controller/FilmController.java
+++ film-service/src/main/java/ua/edu/ukma/cinemax/controller/FilmController.java
@@ -1,6 +1,7 @@
 package ua.edu.ukma.cinemax.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ import java.util.List;
 public class FilmController {
     private final FilmService filmService;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<FilmDto>> getAllFilms() {
         return ResponseEntity.ok(filmService.getAll());
     }
