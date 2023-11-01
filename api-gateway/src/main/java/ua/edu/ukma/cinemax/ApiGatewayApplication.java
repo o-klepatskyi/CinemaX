b54fbebe1eb43baac77b29aca5d22376845fed55
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2023-11-01 19:41:07 +0100

    Spring Cloud first steps

---
+++ api-gateway/src/main/java/ua/edu/ukma/cinemax/ApiGatewayApplication.java
@@ -0,0 +1,25 @@
+package ua.edu.ukma.cinemax;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("film-service", r ->
+                        r.path("/film/**")
+                                .uri("lb://film-service")
+                )
+                .build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
+
--- film-service/src/main/java/ua/edu/ukma/cinemax/controller/FilmController.java
+++ film-service/src/main/java/ua/edu/ukma/cinemax/controller/FilmController.java
@@ -1,8 +1,12 @@
 package ua.edu.ukma.cinemax.controller;
 
+import com.netflix.discovery.*;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.*;
+import org.springframework.context.annotation.*;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -18,6 +22,19 @@ import java.util.List;
 public class FilmController {
     private final FilmService filmService;
     private static final Logger LOGGER = LoggerFactory.getLogger(FilmController.class);
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return String.format(
+                "Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+    }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<FilmDto>> getAllFilms() {
