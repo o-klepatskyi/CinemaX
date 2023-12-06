92e37fd1476dcbcf068f31dbdfea9b564238770e
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2023-12-06 23:39:36 +0100

    eureka server + feign client

--- api-gateway/src/main/java/ua/edu/ukma/cinemax/dto/converter/UserConverter.java
+++
@@ -1,8 +0,0 @@
-package ua.edu.ukma.cinemax.dto.converter;
-
-import ua.edu.ukma.cinemax.commons.GenericConverter;
-import ua.edu.ukma.cinemax.dto.UserDto;
-import ua.edu.ukma.cinemax.persistance.entity.User;
-
-public interface UserConverter extends GenericConverter<UserDto, User> {
-}
--- api-gateway/src/main/java/ua/edu/ukma/cinemax/persistance/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package ua.edu.ukma.cinemax.persistance.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.edu.ukma.cinemax.persistance.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
