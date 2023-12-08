743d751ba3d07fc3879ddd523ee4bcaa1b6ec5ab
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2023-12-08 12:39:19 +0200

    add logs to app/ImageService

--- app/src/main/java/ua/edu/ukma/cinemax/service/impl/ImageServiceImpl.java
+++ app/src/main/java/ua/edu/ukma/cinemax/service/impl/ImageServiceImpl.java
@@ -1,7 +1,8 @@
 package ua.edu.ukma.cinemax.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ua.edu.ukma.cinemax.feign.ImageServiceClient;
@@ -12,12 +13,14 @@ import ua.edu.ukma.cinemax.service.ImageService;
 public class ImageServiceImpl implements ImageService {
     private final ImageServiceClient imageServiceClient;
     private static final String DEFAULT_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg";
+    private static final Logger LOGGER = LoggerFactory.getLogger(ImageServiceImpl.class);
 
     @Override
     public byte[] getFilmImageById(Long tmdbId) {
         try {
             return imageServiceClient.getFilmImage(tmdbId);
-        } catch (Exception ignored) {
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
             return new RestTemplate().getForObject(DEFAULT_IMAGE, byte[].class);
         }
     }
@@ -26,7 +29,8 @@ public class ImageServiceImpl implements ImageService {
     public String getImageLink(Long tmdbId) {
         try {
             return imageServiceClient.getFilmImageURL(tmdbId).getFilmImageUrl();
-        } catch (Exception ignored) {
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
             return DEFAULT_IMAGE;
         }
     }
